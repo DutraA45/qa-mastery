e2db31b12ca476ebc46c52532e92951b3b8a1bd1
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-12-06 15:48:37 -0300

    config-eslint

--- user-api/eslint.config.mjs
+++ user-api/eslint.config.mjs
@@ -5,12 +5,16 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended, {
+  {languageOptions: {globals: globals.node}},
+  pluginJs.configs.recommended,
+  {
     "env": {
-    "jest": true
-}
+      "jest": true,
+    },
+    "rules": {
+      "constructor-super": "error" // Ou "off" se você preferir desativá-la
+    }
   }
-  
 ];
+;
 
