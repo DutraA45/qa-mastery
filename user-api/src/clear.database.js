7412a59640965226f517ef7314c43cc6449765a6
Jefferson Dutra <jeffinho.angelo@gmail.com>
2025-08-01 18:07:30 -0300

    env-mongodb-created

--- user-api/src/clear.database.js
+++ user-api/src/clear.database.js
@@ -3,10 +3,10 @@ const UserRepository = require('./user-repository.js');
 
 (async () => {
   //  const uri = 'mongodb://127.0.0.1:27017/users_db';  // Uso local
-  const uri = 'mongodb://127.0.0.1:27017/users_db'; // Uso Docker Workflow
+  const uri = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/users_db'; // Uso Docker Workflow
   const client = new MongoClient(uri);
   await client.connect();
-  const collection = client.db('users_db').collection('users');
+  const collection = client.db().collection('users');
   const userRepository = new UserRepository(collection);
   await userRepository.deleteAll();
   await client.close();
